617b76ab1286daa063e27df7155818659e5f8798
satej <satejall@gmail.com>
2023-03-28 14:30:59 -0700

    new test added for filesysmter os module

--- deck_test.go
+++ deck_test.go
@@ -1,10 +1,31 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDesk(t *testing.T) {
 	d := newDeck()
 	if len(d) != 16 {
-		t.Errorf("Expected  deck length of 20, but got %v", len(d))
+		t.Errorf("Expected  deck length of 16, but got %v", len(d))
 	}
+	if d[0] != "Spades of Ace" {
+		t.Errorf("Expected first card of Spades of Ace, but got %v", d[0])
+	}
+	if d[len(d)-1] != "Clubs of Four" {
+		t.Errorf("Expected last card of 'Clubs of Four', but got '%v'", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	testingFile := "_decktesting"
+	os.Remove(testingFile)
+	d := newDeck()
+	d.saveToFile(testingFile)
+	loadedDeck := newDeckFromFile(testingFile)
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected  deck length of 16, but got %v", len(loadedDeck))
+	}
+	os.Remove(testingFile)
 }
